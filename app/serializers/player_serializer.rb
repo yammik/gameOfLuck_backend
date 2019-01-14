f1f3fb6690e4e90f70344d8e91921e025f9f6d05
yammik <mkim0514@gmail.com>
2019-01-14 16:12:51 -0500

    heroku pls

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,4 +1,4 @@
 class PlayerSerializer < ActiveModel::Serializer
-  attributes %i(id name age gender latitude longitude no_steps alive)
+  attributes %i(id name age gender latitude longitude no_steps)
   has_many :steps
 end
